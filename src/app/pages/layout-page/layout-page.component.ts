553114806ea2127104e7acfb87ed48eb1870aa40
Deyson Venté <deiandresvent@hotmail.com>
2024-05-05 15:42:30 -0500

    Layout page implementado y funcional

--- src/app/pages/layout-page/layout-page.component.ts
+++ src/app/pages/layout-page/layout-page.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
+import { SidebarItems } from 'src/app/interfaces/sidebaritems';
 
 @Component({
   selector: 'app-layout-page',
-  templateUrl: './layout-page.component.html'
+  templateUrl: './layout-page.component.html',
 })
 export class LayoutPageComponent {
-
+  //*sidebar items para el menu
+  public sidebaritems: SidebarItems[] = [
+    {
+      icon: 'home',
+      name: 'Inicio',
+      path: './',
+    },
+    {
+      icon: 'add',
+      name: 'Agregar',
+      path: './nuevo',
+    },
+  ];
 }

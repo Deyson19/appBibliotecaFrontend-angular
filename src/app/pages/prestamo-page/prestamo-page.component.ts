a0ad6171814bebb73d3889404b277732b394dc12
Deyson Venté <deiandresvent@hotmail.com>
2024-05-05 15:02:26 -0500

    pagina para ver los detalles del prestamo y/o crear nuevo

---
+++ src/app/pages/prestamo-page/prestamo-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prestamo-page',
+  templateUrl: './prestamo-page.component.html'
+})
+export class PrestamoPageComponent {
+
+}

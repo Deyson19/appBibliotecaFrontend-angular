b5c2750f4df3df39272f9a254a4e8d4760947085
Deyson Venté <deiandresvent@hotmail.com>
2024-05-05 15:43:32 -0500

    ruteo configurado y funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,40 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {
+  LayoutPageComponent,
+  ListaPageComponent,
+  PrestamoPageComponent,
+  UpsertPrestamoPageComponent,
+} from './pages';
+import { Error404Component } from './shared/pages/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutPageComponent,
+    children: [
+      {
+        path: '',
+        component: ListaPageComponent,
+      },
+      {
+        path: 'nuevo',
+        component: UpsertPrestamoPageComponent,
+      },
+      {
+        path: 'detalle/:id',
+        component: PrestamoPageComponent,
+      },
+      {
+        path: '**',
+        component: Error404Component,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

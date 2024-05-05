49af4157a65ada9a2d253ff4cbd32c06e4b54fae
Deyson Venté <deiandresvent@hotmail.com>
2024-05-05 15:43:13 -0500

    material module y shared module agregados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import {
   LayoutPageComponent,
   UpsertPrestamoPageComponent,
 } from './pages/';
+import { MaterialModule } from './material/material.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,13 @@ import {
     LayoutPageComponent,
     UpsertPrestamoPageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    SharedModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })

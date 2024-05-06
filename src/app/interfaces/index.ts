73198a35cb9097fc0a8c2048307dc12a92411495
Deyson Venté <deiandresvent@hotmail.com>
2024-05-05 20:15:32 -0500

    archivo de barril para las interfaces

---
+++ src/app/interfaces/index.ts
@@ -0,0 +1,4 @@
+export { PrestamoPorId } from './prestamo.interface';
+export { CrearPrestamo } from './crearprestamo.interface';
+export { ListadoPrestamos } from './listado-prestamos.interface';
+export { SidebarItems } from './sidebaritems';

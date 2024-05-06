f7bf156d264dcb05eae97bfa824538c702fbc0a8
Deyson Venté <deiandresvent@hotmail.com>
2024-05-05 20:14:49 -0500

    listar los prestamos

--- src/app/pages/lista-page/lista-page.component.ts
+++ src/app/pages/lista-page/lista-page.component.ts
@@ -1,53 +1,49 @@
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  ViewChild,
+  inject,
+} from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableDataSource } from '@angular/material/table';
+import { Prestamo } from 'src/app/interfaces/listado-prestamos.interface';
+import { PrestamoService } from 'src/app/services/prestamo.service';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
 @Component({
   selector: 'app-lista-page',
   templateUrl: './lista-page.component.html',
 })
-export class ListaPageComponent implements AfterViewInit {
-  ELEMENT_DATA: PeriodicElement[] = [
-    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-    { position: 11, name: 'Sodium', weight: 22.9897, symbol: 'Na' },
-    { position: 12, name: 'Magnesium', weight: 24.305, symbol: 'Mg' },
-    { position: 13, name: 'Aluminum', weight: 26.9815, symbol: 'Al' },
-    { position: 14, name: 'Silicon', weight: 28.0855, symbol: 'Si' },
-    { position: 15, name: 'Phosphorus', weight: 30.9738, symbol: 'P' },
-    { position: 16, name: 'Sulfur', weight: 32.065, symbol: 'S' },
-    { position: 17, name: 'Chlorine', weight: 35.453, symbol: 'Cl' },
-    { position: 18, name: 'Argon', weight: 39.948, symbol: 'Ar' },
-    { position: 19, name: 'Potassium', weight: 39.0983, symbol: 'K' },
-    { position: 20, name: 'Calcium', weight: 40.078, symbol: 'Ca' },
-  ];
-  displayedColumns: string[] = [
-    'position',
-    'name',
-    'weight',
-    'symbol',
-    'actions',
-  ];
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  public dataSource = new MatTableDataSource<PeriodicElement>(
-    this.ELEMENT_DATA
-  );
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+export class ListaPageComponent implements OnInit {
+  private _prestamoService = inject(PrestamoService);
+  private _snackBar = inject(MatSnackBar);
+  //*Listado de la data
+  public listadoPrestamos: Prestamo[] = [];
+  ngOnInit() {
+    this.cargarPrestamos();
+  }
+  displaySnackbar(mensaje: string, duracion: number) {
+    this._snackBar.open(mensaje, 'Close', {
+      duration: duracion,
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+    });
+  }
+  cargarPrestamos() {
+    this._prestamoService.getPrestamos().subscribe(
+      (data) => {
+        if (data.isSuccess) {
+          this.displaySnackbar(data.message, 3500);
+          this.listadoPrestamos = data.result;
+        }
+      },
+      (error: HttpErrorResponse) => {
+        console.log({ error });
+        this.displaySnackbar(error.message, 5000);
+      }
+    );
   }
 
   hacerLog(data: string) {

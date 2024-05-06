4c7f90822a41d49255770558a54af766c8b5d22c
Deyson Venté <deiandresvent@hotmail.com>
2024-05-05 23:32:41 -0500

    metodo para actualizar implementado

--- src/app/services/prestamo.service.ts
+++ src/app/services/prestamo.service.ts
@@ -2,7 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environments';
-import { CrearPrestamo, ListadoPrestamos, PrestamoPorId } from '../interfaces/';
+import {
+  CrearPrestamo,
+  ListadoPrestamos,
+  PrestamoPorId,
+  PrestamoCreado,
+  ActualizarPrestamo,
+  Prestamo,
+} from '../interfaces/';
 
 @Injectable({
   providedIn: 'root',
@@ -26,20 +33,28 @@ export class PrestamoService {
       `${this.baseUrl}/Prestamo/${idPrestamo}`
     );
   }
-  postPrestamo(nuevoPrestamo: CrearPrestamo) {
+  postPrestamo(nuevoPrestamo: CrearPrestamo): Observable<PrestamoCreado> {
     if (!nuevoPrestamo) {
       throw new Error('No se ha ingresado un modelo valido');
     }
-    return this._http.post<ListadoPrestamos>(
-      `${this.baseUrl}/api/Prestamo`,
+    return this._http.post<PrestamoCreado>(
+      `${this.baseUrl}/Prestamo`,
       nuevoPrestamo
     );
   }
   deletePrestamo(idPrestamo: number) {
-    //!Actualmente no existe un metodo delete en el api, pero se hace la semejanza
     if (!idPrestamo) {
       throw new Error(this.idIncorrecto);
     }
     return this._http.delete<boolean>(`${this.baseUrl}/Prestamo/${idPrestamo}`);
+  }
+  putPrestamo(actualizar: ActualizarPrestamo): Observable<Prestamo> {
+    if (!actualizar) {
+      throw new Error('No se posible realizar la solicitud');
+    }
+    return this._http.put<Prestamo>(
+      `${this.baseUrl}/Prestamo/${actualizar.id}`,
+      actualizar
+    );
   }
 }

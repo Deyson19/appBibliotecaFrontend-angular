ac8b85003e807260365dc35d3b8d3d5b55470f75
Deyson Venté <deiandresvent@hotmail.com>
2024-05-05 23:33:45 -0500

    Nuevos modelos agregados al archivo de barril

--- src/app/interfaces/crearprestamo.interface.ts
+++ src/app/interfaces/crearprestamo.interface.ts
@@ -1,8 +1,22 @@
 export interface CrearPrestamo {
   // 6
-  Isbn: string;
+  isbn: string;
   // 10
-  IdentificacionUsuario: string;
+  identificacionUsuario: string;
   // 1
-  TipoUsuarioId: number;
+  tipoUsuarioId: number;
+}
+export interface ActualizarPrestamo {
+  id: number;
+
+  // 6
+  isbn: string;
+  // 10
+  identificacionUsuario: string;
+  // 1
+  tipoUsuarioId: number;
+}
+export interface TipoUsuario {
+  id: number;
+  nombre: string;
 }
--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -1,3 +1,7 @@
+export { TipoUsuario } from './crearprestamo.interface';
+export { Prestamo } from 'src/app/interfaces/listado-prestamos.interface';
+export { ActualizarPrestamo } from './crearprestamo.interface';
+export { PrestamoCreado } from './prestamocreado.interface';
 export { PrestamoPorId } from './prestamo.interface';
 export { CrearPrestamo } from './crearprestamo.interface';
 export { ListadoPrestamos } from './listado-prestamos.interface';

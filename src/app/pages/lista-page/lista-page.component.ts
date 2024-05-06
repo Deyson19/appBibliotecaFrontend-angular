75d845e2416586c1c7010ec3e880fa6f6befecf9
Deyson Venté <deiandresvent@hotmail.com>
2024-05-06 11:20:47 -0500

    Mejora del diseño y funcionalidad de ver detalle del item y eliminar.

--- src/app/pages/lista-page/lista-page.component.ts
+++ src/app/pages/lista-page/lista-page.component.ts
@@ -1,15 +1,10 @@
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import {
-  AfterViewInit,
-  Component,
-  OnInit,
-  ViewChild,
-  inject,
-} from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { Component, OnInit, inject } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatTableDataSource } from '@angular/material/table';
-import { Prestamo } from 'src/app/interfaces/listado-prestamos.interface';
+import { filter, switchMap } from 'rxjs';
+import { ConfirmDialogComponent } from 'src/app/components/confirm-dialog/confirm-dialog.component';
+import { PrestamoPorId, Prestamo } from 'src/app/interfaces';
 import { PrestamoService } from 'src/app/services/prestamo.service';
 
 @Component({
@@ -19,6 +14,9 @@ import { PrestamoService } from 'src/app/services/prestamo.service';
 export class ListaPageComponent implements OnInit {
   private _prestamoService = inject(PrestamoService);
   private _snackBar = inject(MatSnackBar);
+  private _prestamoActual?: PrestamoPorId;
+  private dialog = inject(MatDialog);
+
   //*Listado de la data
   public listadoPrestamos: Prestamo[] = [];
   ngOnInit() {
@@ -40,12 +38,42 @@ export class ListaPageComponent implements OnInit {
         }
       },
       (error: HttpErrorResponse) => {
-        console.log({ error });
         this.displaySnackbar('No se pudo realizar la petición', 5000);
       }
     );
   }
 
+  eliminar(id: number) {
+    if (!id) {
+      throw new Error('No se ha enviado id');
+    }
+    this._prestamoService.getPrestamo(id).subscribe((data) => {
+      if (data.id) {
+        this._prestamoActual = data;
+      }
+    });
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this._prestamoActual,
+    });
+
+    dialogRef
+      .afterClosed()
+      .pipe(
+        filter((confirm: boolean) => confirm),
+        switchMap(() =>
+          this._prestamoService.deletePrestamo(this._prestamoActual!.id)
+        ),
+        filter((wasDelete: boolean) => wasDelete)
+      )
+      .subscribe((x) => {
+        this._snackBar.open('Se ha eliminado el registro', 'Ok', {
+          duration: 2750,
+        });
+        setTimeout(() => {
+          this.cargarPrestamos();
+        }, 3000);
+      });
+  }
   hacerLog(data: string) {
     console.log(data);
   }
